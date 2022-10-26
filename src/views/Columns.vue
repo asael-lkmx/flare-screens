26b7394cd0a64c4aab1522f7cb23a2268a2cfcb7
Asael <asael.medina@lkmx.io>
2022-10-26 09:36:51 -0700

    Change background color in columns view

--- src/views/Columns.vue
+++ src/views/Columns.vue
@@ -4,40 +4,40 @@
     <h2>Mode: Full</h2>
     <h3>weight: Normal</h3>
     <Column number="2" mode="full">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
     <h3>weight: right</h3>
     <Column number="2" mode="full" weight="right">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
     <h2>Mode: Normal</h2>
     <h3>weight: Normal</h3>
     <Column number="2">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
     <h3>weight: right</h3>
     <Column number="2" weight="right">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
     <h2>Mode: Slim</h2>
     <h3>weight: Normal</h3>
     <Column number="2" mode="slim">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
     <h3>weight: right</h3>
     <Column number="2" mode="slim" weight="right">
-      <Block>Column 1</Block>
-      <Block>Column 2</Block>
+      <Block class="left">Column 1</Block>
+      <Block class="right">Column 2</Block>
     </Column>
 
   </Page>
@@ -55,10 +55,11 @@ export default {
   .--flare-block {
     color: white;
     font-weight: bold;
-    &:first-child {
+    background-color: #00D0F6;
+    &.left :last-child {
       background-color: #0B0E29;
     }
-    &:last-child {
+    &.right :last-child {
       background-color: #1324A0;
     }
   }
